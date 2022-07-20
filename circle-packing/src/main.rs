2d6d67fcecc08d688daec12dfa1895963d263d1a
nate-sys <nathandawit555@gmail.com>
2022-07-20 08:27:27 +0300

    restructure circle-packing model

--- circle-packing/src/main.rs
+++ circle-packing/src/main.rs
@@ -1,45 +1,41 @@
 use nannou::prelude::*;
+
 mod circle;
 use circle::Circle;
-const W: f32 = 480.0;
-const H: f32 = 540.0;
-const ATTEMPTS: u32 = 6000;
-const MIN_R: f32 = 4.0;
-const MAX_R: f32 = 64.0;
-const COLORS: [(u8, u8, u8); 9] = [
-    (55, 6, 23),
-    (106, 4, 15),
-    (157, 2, 8),
-    (208, 0, 0),
-    (220, 47, 2),
-    (232, 93, 4),
-    (244, 140, 6),
-    (250, 163, 7),
-    (255, 186, 8),
-];
+// global colors
+const COLORS: [(u8, u8, u8); 3] = [(255, 231, 154), (255, 214, 122), (250, 191, 85)];
+
+struct Settings {
+    w: f32,
+    h: f32,
+    attempts: u32,
+    min_r: f32,
+    max_r: f32,
+}
+
 fn main() {
     nannou::app(model).update(update).run();
 }
+
 struct Model {
     circles: Vec<Circle>,
-    minimum_radius: f32,
-    maximum_radius: f32,
-    density: u32,
+    settings: Settings,
 }
-fn get_new_circles(minimum: f32, maximum: f32, density: u32) -> Vec<Circle> {
+
+fn get_new_circles(s: &Settings) -> Vec<Circle> {
     let mut c = Vec::new();
-    for _i in 0..density {
-        let radius = map_range(random::<f32>(), 0.0, 1.0, minimum, maximum);
-        let curr_color = COLORS[map_range(radius, minimum, maximum, 0, COLORS.len()) as usize];
+    for _i in 0..s.attempts {
+        let radius = map_range(random::<f32>(), 0.0, 1.0, s.min_r, s.max_r);
+        let curr_color = COLORS[map_range(radius, s.min_r, s.max_r, 0, COLORS.len()) as usize];
         let new_circle = Circle::new(
-            random::<f32>() * W - W / 2.0,
-            random::<f32>() * H - H / 2.0,
+            random::<f32>() * s.w - s.w / 2.0,
+            random::<f32>() * s.h - s.h / 2.0,
             radius,
             srgb8(curr_color.0, curr_color.1, curr_color.2),
         );
         let mut has_collided = false;
         for cir in c.iter() {
-            if new_circle.colliding(cir, W, H) {
+            if new_circle.colliding(cir, s.w, s.h) {
                 has_collided = true;
             }
         }
@@ -49,6 +45,7 @@ fn get_new_circles(minimum: f32, maximum: f32, density: u32) -> Vec<Circle> {
     }
     c
 }
+
 fn model(app: &App) -> Model {
     let _window = app
         .new_window()
@@ -56,56 +53,55 @@ fn model(app: &App) -> Model {
         .view(view)
         .build()
         .unwrap();
+    let s = Settings {
+        w: 400.0,
+        h: 400.0,
+        attempts: 5000,
+        min_r: 4.0,
+        max_r: 64.0,
+    };
     Model {
-        circles: get_new_circles(MIN_R, MAX_R, ATTEMPTS),
-        minimum_radius: MIN_R,
-        maximum_radius: MAX_R,
-        density: ATTEMPTS,
+        circles: get_new_circles(&s),
+        settings: s,
     }
 }
+
 fn update(_app: &App, _model: &mut Model, _update: Update) {}
+
 fn key_pressed(_app: &App, model: &mut Model, key: Key) {
+    let mut get_new = true;
     match key {
-        Key::Space => {
-            model.circles =
-                get_new_circles(model.minimum_radius, model.maximum_radius, model.density);
-        }
+        Key::Space => {}
         Key::Down => {
-            model.minimum_radius -= 2.0;
-            model.circles =
-                get_new_circles(model.minimum_radius, model.maximum_radius, model.density);
+            model.settings.min_r -= 2.0;
         }
         Key::Up => {
-            model.minimum_radius += 2.0;
-            model.circles =
-                get_new_circles(model.minimum_radius, model.maximum_radius, model.density);
+            model.settings.min_r += 2.0;
         }
         Key::Left => {
-            model.maximum_radius -= 2.0;
-            model.circles =
-                get_new_circles(model.minimum_radius, model.maximum_radius, model.density);
+            model.settings.max_r -= 2.0;
         }
         Key::Right => {
-            model.maximum_radius += 2.0;
-            model.circles =
-                get_new_circles(model.minimum_radius, model.maximum_radius, model.density);
+            model.settings.max_r += 2.0;
         }
         Key::D => {
-            model.density -= 100;
-            model.circles =
-                get_new_circles(model.minimum_radius, model.maximum_radius, model.density);
+            model.settings.attempts -= 100;
         }
         Key::I => {
-            model.density += 100;
-            model.circles =
-                get_new_circles(model.minimum_radius, model.maximum_radius, model.density);
+            model.settings.attempts += 100;
         }
-        _ => {}
+        _ => {
+            get_new = false;
+        }
+    }
+    if get_new {
+        model.circles = get_new_circles(&model.settings);
     }
 }
+
 fn view(app: &App, model: &Model, frame: Frame) {
     let draw = app.draw();
-    draw.background().color(srgb8(0, 0, 0));
+    draw.background().color(srgb8(110, 131, 78));
     for c in model.circles.iter() {
         draw.ellipse()
             .xy(c.position)
